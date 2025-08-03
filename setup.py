1f0273235dcea9ca14e5c348bbca77dae9bb91f8
zyxasaduzzaman <zyxmdasaduzzaman@gmail.com>
2025-08-04 00:46:07 +0600

    change name of library

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
-    name='colorMide',
-    version='0.1.1',
+    name='colorMind',
+    version='0.1.2',
     description='A Python library for color theory, moods, and palettes',
     long_description=long_description,
     long_description_content_type='text/markdown',
