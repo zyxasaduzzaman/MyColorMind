6d17a3c86b7305f9ee2963f3d1df736eb41106b2
zyxasaduzzaman <zyxmdasaduzzaman@gmail.com>
2025-08-04 00:31:22 +0600

    v-change

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='colorMindAsad',
-    version='0.1.0',
+    version='0.1.1',
     description='A Python library for color theory, moods, and palettes',
     long_description=long_description,
     long_description_content_type='text/markdown',
